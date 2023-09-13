2c1048169751f7450d168c8dd95a342a9229f297
Odong Sunday <sandieo.2020@gmail.com>
2023-09-13 22:20:58 +0300

    add file viewer

--- src/core/api/baseURL.ts
+++ src/core/api/baseURL.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const baseUrl = "/api/-default-/public/alfresco/versions/1/";
+// /api/-default-/public/alfresco/versions/1/nodes/{documentId}/content'
 export const term = "&include=properties,path,isLocked,isDirectLinkEnabled,aspectNames,isFavorite,allowableOperations,isLink";
 const credentials = { username: 'admin', password: 'admin' };
 
