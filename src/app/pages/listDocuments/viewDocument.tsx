2c1dfbf553e1c90b84c65beafd0c1c95f3138167
Odong Sunday <sandieo.2020@gmail.com>
2023-09-13 23:09:58 +0300

    update modal

--- src/app/pages/listDocuments/viewDocument.tsx
+++ src/app/pages/listDocuments/viewDocument.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { baseUrl } from '../../../core/api/baseURL';
 import { IEntry } from './interface';
+import { Box, Button, Typography } from '@mui/material';
 
 interface IDocumentViewer {
   entry: IEntry
@@ -9,6 +10,13 @@ interface IDocumentViewer {
 
 const DocumentViewer = ({ entry }: IDocumentViewer) => {
   const [documentContent, setDocumentContent] = useState<Uint8Array | null>(null);
+  const [iframeStyles, setIframeStyles] = useState<{
+    pointerEvents: string;
+    userSelect: string;
+  }>({
+    pointerEvents: 'none',
+    userSelect: 'none'
+  })
 
   useEffect(() => {
     const apiUrl = `${baseUrl}nodes/{documentId}/content`;
@@ -31,22 +39,35 @@ const DocumentViewer = ({ entry }: IDocumentViewer) => {
       });
   }, []);
 
+  const onIframeClick = () => console.log('Iframe clicked')
+
   return (
-    <div>
-      <h2>Document Viewer</h2>
+    <Box>
+      <Box sx={{ width: '100%', display: 'flex', py: 2 }}>
+        <Typography>{entry.name}</Typography>
+        <Button
+          onClick={() => {
+            setIframeStyles({
+              pointerEvents: '',
+              userSelect: ''
+            })
+          }}
+          sx={{ marginLeft: 'auto' }} variant='contained'>View Document</Button>
+      </Box>
       {documentContent && (
         <iframe
-               src={`data:application/pdf;base64,${btoa(String.fromCharCode(...Array.from(documentContent)))}`}
-               title="Document"
-               width="100%"
-               height="600px"
-               style={{
-                pointerEvents: 'none',
-                userSelect: 'none',
-              }}
-             />
+          onClick={onIframeClick}
+          src={`data:application/pdf;base64,${btoa(String.fromCharCode(...Array.from(documentContent)))}`}
+          title="Document"
+          width="100%"
+          height="600px"
+          style={{
+            pointerEvents: iframeStyles.pointerEvents as any,
+            userSelect: iframeStyles.userSelect as any,
+          }}
+        />
       )}
-    </div>
+    </Box>
   );
 }
 export default DocumentViewer
