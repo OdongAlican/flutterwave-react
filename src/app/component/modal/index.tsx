5a53da3cc0cbd0586bd8f1b7d8cf0ecee04142bb
Odong Sunday <sandieo.2020@gmail.com>
2023-09-14 23:10:30 +0300

    update modal

--- src/app/component/modal/index.tsx
+++ src/app/component/modal/index.tsx
@@ -15,7 +15,7 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '80%',
-    height: '80%',
+    height: '100%',
     bgcolor: 'background.paper',
     boxShadow: 24,
     pt: 2,
